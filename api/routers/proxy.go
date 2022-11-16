b6c525c45bab3b26815122f0b9db5a51104576e3
eagle9527 <eagle@qq.com>
2022-11-16 16:01:22 +0800

    删除无用代码

--- api/routers/proxy.go
+++ api/routers/proxy.go
@@ -7,6 +7,5 @@ import (
 
 func ProxyApiRouter(r *gin.RouterGroup) {
 	ProxyRouter := r.Group("kubernetes/proxy")
-	ProxyRouter.Any("clusters", k8s.ListCluster)
 	ProxyRouter.Any("/:cluster_id/*path", k8s.K8sAPIProxy)
 }
